8c3f67d5621a2c04eae0cfbf2a0b4187a33d721d
Micheala Palmer <michealapalmer@gmail.com>
2021-12-06 21:47:17 -0500

    add a POST route to receive the form submission

--- express_server.js
+++ express_server.js
@@ -8,6 +8,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -38,6 +40,10 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
