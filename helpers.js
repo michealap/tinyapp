cbfabe0551012713c387f784bb7c6a08523665bd
Micheala Palmer <michealapalmer@gmail.com>
2021-12-10 18:07:32 -0500

    helper functions module

---
+++ helpers.js
@@ -0,0 +1,42 @@
+//Helper Functions
+
+//simulate generating a 'unique' shortURL
+const generateRandomString = function() {
+  let randomString = "";
+  for (let i = 0; i < 6; i++) {
+    const randomCharCode = Math.floor(Math.random() * 26 + 97);
+    const randomChar = String.fromCharCode(randomCharCode);
+    randomString += randomChar;
+  }   return randomString;
+};
+
+const findUserByEmail = (email, users) => {
+  //console.log(email, users);
+  for (const id in users) {
+    const currentUser = users[id];
+    if (currentUser.email === email) {
+      return currentUser;
+    }
+    return null;
+  }
+};
+
+const urlsForUser = function(urlDatabase, id) {
+  let urlsObj = {};
+  for (let shortUrl in urlDatabase) {
+    if (urlDatabase[shortUrl].userID === id) {
+      urlsObj[shortUrl] = urlDatabase[shortUrl].longURL;
+    }
+  }
+  return urlsObj;
+};
+
+const fetchUrlDatabase = function(urlDatabase) {
+  let urlsObj = {};
+  for (let urls in urlDatabase) {
+    urlsObj[urls] = urlDatabase[urls].longURL;
+  }
+  return urlsObj;
+};
+
+module.exports = { generateRandomString, findUserByEmail, urlsForUser, fetchUrlDatabase};
