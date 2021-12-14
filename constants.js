4100f87b17b4a95902b7a672489ef6b2ad7d1791
Micheala Palmer <michealapalmer@gmail.com>
2021-12-13 23:33:51 -0500

    separate data stores in own file

---
+++ constants.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcryptjs');
+
+//data stores
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: bcrypt.hashSync("purple", 10)
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: bcrypt.hashSync("dishwasher-funk", 10)
+  }
+};
+
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
+  }
+};
+
+module.exports = {users, urlDatabase};
